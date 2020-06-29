67b46c1217a8c6d6ceb3be98c9411bbf3251ea2f
albanaguilar <albanaguilar1@gmail.com>
2020-06-28 23:17:12 -0500

    First approach, need design

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+// FEATURE DISCOVERY
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tap-target');
+    var instances = M.TapTarget.init(elems);
+  });
+// END FEATURE DISCOVERY
+
+
+
+
+
